895b943db11443a5ba39095c02c80101e4c6bbed
Aaron Brodersen <aaron@abrodersen.com>
2021-03-27 19:58:47 -0500

    Install bootctl update script

--- setup-dracut.sh
+++ setup-dracut.sh
@@ -4,3 +4,4 @@ set -e
 install -Dm0644 -t /etc/dracut.conf.d/ dracut-defaults.conf
 install -Dm0755 -t /usr/local/bin dracut-install dracut-remove
 install -Dm0644 -t /etc/pacman.d/hooks/ 60-dracut-remove.hook 90-dracut-install.hook
+install -Dm0755 -t /usr/local/bin bootctl-update
